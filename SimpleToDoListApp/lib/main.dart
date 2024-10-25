ce8735921d23c08c61ba1579bd06a7678a88425b
Mudiwa Rangarirai Mandivenga <Mudiwam@dicomm.co.zw>
2024-10-25 10:53:25 +0200

    Initial commit of Simple To-Do List App

---
+++ SimpleToDoListApp/lib/main.dart
@@ -0,0 +1,103 @@
+ 
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Simple To-Do List',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const ToDoListScreen(),
+    );
+  }
+}
+
+class ToDoListScreen extends StatefulWidget {
+  const ToDoListScreen({Key? key}) : super(key: key);
+
+  @override
+  _ToDoListScreenState createState() => _ToDoListScreenState();
+}
+
+class _ToDoListScreenState extends State<ToDoListScreen> {
+  final List<String> _tasks = [];
+  final TextEditingController _taskController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTasks();
+  }
+
+  Future<void> _loadTasks() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _tasks.addAll(prefs.getStringList('tasks') ?? []);
+    });
+  }
+
+  Future<void> _saveTasks() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setStringList('tasks', _tasks);
+  }
+
+  void _addTask() {
+    if (_taskController.text.isNotEmpty) {
+      setState(() {
+        _tasks.add(_taskController.text);
+        _taskController.clear();
+      });
+      _saveTasks();
+    }
+  }
+
+  void _removeTask(int index) {
+    setState(() {
+      _tasks.removeAt(index);
+    });
+    _saveTasks();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('To-Do List')),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _taskController,
+              decoration: const InputDecoration(labelText: 'New Task'),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _tasks.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(_tasks[index]),
+                  trailing: IconButton(
+                    icon: const Icon(Icons.delete),
+                    onPressed: () => _removeTask(index),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addTask,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
